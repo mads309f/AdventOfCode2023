2e0ef034248a3672f6dce6325a3c2e51531c00a8
mads309f <2mads.v.jensen@gmail.com>
2023-12-05 09:03:41 +0100

    simplification

--- 2023/5_Dec/b.py
+++ 2023/5_Dec/b.py
@@ -14,41 +14,37 @@ with open('input.txt', 'r') as f:
             map = line.strip().split(' ')
             maps[-1].append((int(map[0]), int(map[1]), int(map[2])))
 
-# sort based on mapping[1]
+# sort map based on source
 for m in maps:
     m.sort(key=lambda x: x[1])
 
 # add missing ranges
 for m in maps:
-    for j in range(len(m)):
-        if j == 0:
-            continue
-        if m[j-1][1] + m[j-1][2] != m[j][1]:
-            m_len = m[j][1] - (m[j-1][1] + m[j-1][2])
-            m.insert(j, (m[j-1][1] + m[j-1][2], m[j-1][1] + m[j-1][2], m_len))
+    for j in range(1, len(m)):
+        end = m[j-1][1] + m[j-1][2]
+        if end != m[j][1]:
+            m.insert(j, (end, end,  m[j][1] - end))
 
-def get_locations(start, l, mi):
+def get_locations(start, l, depth, i):
     if l <= 0:
         return []
-    if mi == len(maps):
+    if depth == len(maps):
         return [start]
-    locs = []
-    for m in maps[mi]:
 
-        if start + l >= m[1] and start < m[1] + m[2]: 
-            locs.extend(
-                get_locations(start, min(l, m[1] - start), mi) +
-                get_locations(m[0] + abs(start - m[1]), min(l, m[1] + m[2] - start), mi+1) +
-                get_locations(m[1] + m[2], start + l - m[1] - m[2], mi)
-            )
-            return locs
+    m = maps[depth][i]
+    if start + l >= m[1] and start < m[1] + m[2]: 
+        return (get_locations(start, min(l, m[1] - start), depth, 0) +
+                get_locations(m[0] + abs(start - m[1]), min(l, m[1] + m[2] - start), depth+1, 0) +
+                get_locations(m[1] + m[2], start + l - m[1] - m[2], depth, 0))
 
-    locs.extend(get_locations(start, l, mi+1))
-    return locs
+    if i == len(maps[depth]) - 1:
+        return get_locations(start, l, depth+1, 0)
+
+    return get_locations(start, l, depth, i+1)
 
 locations = []
 for i in range(0, len(seeds), 2):
-    loc = get_locations(seeds[i], seeds[i+1], 0)
+    loc = get_locations(seeds[i], seeds[i+1], 0, 0)
     locations.extend(loc)
 
 print(min(locations))
